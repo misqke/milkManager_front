02330f9b37690e9e2cc2f924418196cb047076b7
Misqke <misqke.rust@yahoo.com>
2022-08-05 20:55:13 -0400

    wake api call

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,10 @@
+import axios from "axios";
 import login from "./login";
 import { submitInventory, submitOrder, getConfirmation } from "./submit";
 
-export { login, submitInventory, submitOrder, getConfirmation };
+const startApi = async () => {
+  const { data } = await axios.get("api/start");
+  return data;
+};
+
+export { login, submitInventory, submitOrder, getConfirmation, startApi };
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { HomePage, Loading } from "../components";
+import { startApi } from "../actions";
 
 const Home = () => {
   const [loading, setLoading] = useState(true);
@@ -9,10 +10,16 @@ const Home = () => {
   const user = useSelector((state) => state.user);
 
   useEffect(() => {
-    if (!user.name.length) {
-      router.push("/login");
-    }
-    setLoading(false);
+    const wakeApi = async () => {
+      const data = await startApi();
+      if (!user.name.length) {
+        router.push("/login");
+      }
+      if (data) {
+        setLoading(false);
+      }
+    };
+    wakeApi();
   }, [user, router]);
 
   return <>{loading ? <Loading /> : <HomePage user={user} />}</>;
